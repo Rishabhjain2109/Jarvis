449ed2be3f1f1c17fbfe52fd0a563e29c6fc7d74
rishabh jain <rishabhj8706@gmail.com>
2026-07-22 21:56:54 +0530

    add llm agents and connect it with each of node of graph,add controllers and routes

--- backend/services/agent/agents/chat.agent.js
+++ backend/services/agent/agents/chat.agent.js
@@ -1,3 +1,21 @@
-export const chatAgent=async (params)=>{
-    
+import { getModel } from "../config/llmModels"
+
+export const chatAgent=async (state)=>{
+    const llm=getModel("chat")
+    const prompt="You are CortexAI, an intelligent AI assistant."
+    const response=(await llm).invoke([
+        {
+            "role":"system",
+            "content":prompt,
+        },
+        {
+            "role":"human",
+            "content":state.prompt
+        }
+    ])
+
+    return{
+        ...state,
+        aiResponse:response.content
+    } 
 }
--- backend/services/agent/graph/router.js
+++ backend/services/agent/graph/router.js
@@ -1,3 +1,71 @@
-export const router=async (params)=>{
+import { chatAgent } from "../agents/chat.agent"
+import { serachAgent } from "../agents/search.agent"
+import { getModel } from "../config/llmModels"
+
+export const router = async (state) => {
+    const llm =await getModel("router")
+    const prompt = `You are an agent router.
+    Available agents:
+    -chat
+    -serach
+    -coding
+    -pdf
+    -ppt
+    -vision
+
+    Rules:
+
+    chat:
+    General conversation,
+    explainations,
+    learning,
+    questions.
+
+    search:
+    Current events,
+    latest information,
+    news,
+    recent developments,
+    internet lookup.
+
+    coding:
+    Generate code,
+    debug code,
+    build projects,
+    architecture,
+    API design.
+
+    pdf:
+    Questions about generate PDFs
+    or document context.
+
+    ppt:
+    Questions about generate ppts
+    or ppt context.
     
+    vision:
+    Generate image,
+    create image.
+
+    Return ONLY one word:
+
+    chat
+    search
+    coding
+    pdf
+    ppt
+    vision
+
+    User Query:
+    ${state.prompt}
+    `
+
+    const response=await llm.invoke(prompt)
+    console.log(response)
+    return{
+        ...state,
+        agent:response.content
+              .trim()
+              .tolowerCase()
+    }
 }
